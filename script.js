3ea9891ff4e0af9af637cbb6c960427532c215c2
Tezzish <ishanpahwa14@gmail.com>
2022-08-14 01:53:29 +0530

    yes

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ class GameHolder {
     playGame(){
         //gets computer choice
         this.computerChoice = this.getComputerChoice();
+
+        //console.log(this.computerChoice);
+        //console.log(this.humanChoice);
         
         //human chose rock
         if(this.humanChoice == "rock"){
@@ -96,37 +99,40 @@ class GameHolder {
     }
 
     //updates the score on the bottom
-    function update(){
+    update(){
         human.textContent = "Your score: " + this.score[0];
         comp.textContent = "Computer score: " + this.score[1];
     }
 
     //updates what the computer chose
-    function updateComputerChoice(){
+    updateComputerChoice(){
         computerChoice.textContent = "The computer chose: " + this.computerChoice;
     }
 
     //updates the text after the entire game is finished.
-    function finalText(){
+    finalText(){
         msg.textContent = "Max number of games reached";
         if(this.score[0] > this.score[1]){
-            msg.style = "color: green;"
+            scores.style = "color: green;"
         }
 
         else if (this.score[0] < this.score[1]){
-            msg.style = "color: red;"
+            scores.style = "color: red;"
         }
         else{
-            msg.style = "color: red;"
+            console.log(this.score[0]);
+            console.log(this.score[1]);
+            scores.style = "color: yellow;"
         }
     }
 
     //plays the game when a button is pressed
-    function clicked(str){
+    clicked(str){
         this.humanChoice = str;
+        let ret = this.playGame();
         this.updateComputerChoice();
         this.update();
-        return this.playGame()
+        return ret;
     }
 
 }
@@ -136,8 +142,9 @@ let i = 0;
 let gameHolder = new GameHolder();
 
 rock.onclick = () => {
-    if(i < 5){
-        i = i + gameHolder.clicked(rock);
+    if(i < 4){
+        i = i + gameHolder.clicked("rock");
+        console.log(i);
     }
     else{
         gameHolder.finalText();
@@ -145,8 +152,8 @@ rock.onclick = () => {
 }
 
 paper.onclick = () => {
-    if(i < 5){
-        i = i + gameHolder.clicked(rock);
+    if(i < 4){
+        i = i + gameHolder.clicked("paper");
     }
     else{
         gameHolder.finalText();
@@ -154,8 +161,8 @@ paper.onclick = () => {
 }
 
 scissors.onclick = () => {
-    if(i < 5){
-        i = i + gameHolder.clicked(rock);
+    if(i < 4){
+        i = i + gameHolder.clicked("scissors");
     }
     else{
         gameHolder.finalText();
